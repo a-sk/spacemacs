1d755c531c340780f3ca808a41135cc5176a0377
justbur <justin@burkett.cc>
2016-01-02 20:57:57 -0500

    core: Remove ido from doc of dotspacemacs-completion-tool

--- core/core-dotspacemacs.el
+++ core/core-dotspacemacs.el
@@ -158,9 +158,9 @@ start.")
 `find-files' (SPC f f) is replaced.")
 
 (defvar dotspacemacs-completion-tool 'helm
-  "Completion tool to use. Default is helm. Choices are helm, ivy
-and ido. When ivy is chosen, substitutes are chosen for helm
-related packages.")
+  "Completion tool to use. Default is helm. Choices are helm and
+ivy. When ivy is chosen, substitutes are chosen for helm related
+packages.")
 
 (defvar dotspacemacs-helm-resize nil
   "If non nil, `helm' will try to minimize the space it uses.")
